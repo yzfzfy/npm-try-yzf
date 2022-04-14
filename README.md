3052f627d28796a26a9329d8ea7ca6afc3a9c7ae
402674280@qq.com <402674280@qq.com>
2022-04-14 18:27:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# npm-try-yzf
---
+++ index.js
@@ -0,0 +1,6 @@
+exports.printMsg = function () {
+    console.log('This is a message from the demo package');
+    console.log('This is a message from the demo package');
+    console.log('This is a message from the demo package stable');
+    console.log('This is a message from the demo package version');
+};
